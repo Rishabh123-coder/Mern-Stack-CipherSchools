f393ecc1c0b656ca2a5bb442b5aaa6ba77a8d9a4
Rishabh123-coder <65180333+Rishabh123-coder@users.noreply.github.com>
2021-06-20 17:41:15 +0530

    blog api added

---
+++ Assignment 7/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user-routes');
+const adminRoutes = require('./routes/admin-routes');
+const HttpError = require('./utils/http-error');
+
+// Configuration statements
+const port = 3000;
+app.use(bodyParser.json());
+
+
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Role');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+})
+
+
+// Routing
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+
+
+// Error Handling
+app.use((req,res,next) => {
+  const error = new HttpError('Page not found',404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.code);
+  res.json({message: error.message || 'Unknown error occured' , code: error.code });
+});
+
+
+mongoose.connect('mongodb+srv://rishabhgaur:vir2ris3kan@mernstackcluster.56koz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }).then(() => {
+    app.listen(port, () => {
+      console.log(`App running on http://localhost:${port}`)
+    });
+  }).catch(err => {
+    console.log(err);
+  });
